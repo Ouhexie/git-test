8b9fe80c204bc0c2dd7bf8f28160dae4bcb6cf41
NanJi <2413037348@qq.com>
2025-05-28 16:24:12 +0800

    three commit

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git! 1");
         System.out.println("Hello Git! 2");
+        System.out.println("Hello Git! 3");
     }
 }
